2ed2b51f3424c75be9dfe7fc6a327182ad9a0a88
robert longo <robertflongo@gmail.com>
2019-05-11 23:18:49 -0400

    Added: main file with memory implementation for testing

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/rlongo/ambrosia/api"
+	"github.com/rlongo/ambrosia/app"
+	"github.com/rlongo/ambrosia/storage/memory"
+	"github.com/urfave/negroni"
+)
+
+func getOSVariable(key string) string {
+	if v := os.Getenv(key); len(v) > 0 {
+		return v
+	}
+
+	panic(fmt.Sprintf("env %s isn't set!", key))
+}
 
 func main() {
-	fmt.Println("Hello World")
+
+	port := getOSVariable("PORT")
+
+	recipesDB := api.Recipes{}
+
+	storageService := memory.AmbrosiaStorageMemory{RecipesDB: recipesDB}
+	middleware := negroni.New(
+		negroni.NewRecovery(),
+		negroni.NewLogger(),
+	)
+	router := app.NewRouter(&storageService, middleware)
+
+	log.Printf("listening on IPv4 address \"0.0.0.0\", port %s", port)
+	log.Printf("listening on IPv6 address \"::\", port %s", port)
+
+	s := &http.Server{
+		Addr:           ":" + port,
+		Handler:        router,
+		WriteTimeout:   time.Second * 15,
+		ReadTimeout:    time.Second * 15,
+		IdleTimeout:    time.Second * 60,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	log.Fatal(s.ListenAndServe())
 }
